29c5c884b7f92e02ac71c576cb9569ea78b49dda
nephewtom <tomasorti@gmail.com>
2025-05-04 18:07:01 +0200

    Tested on MacOS

--- build-mac.sh
+++ build-mac.sh
@@ -1,6 +1,6 @@
-rm -f ./ray-cube
+rm -f ./cotw
 
 RAYLIB_DIR=../rolling-cube/raylib/src
 
-clang++ -g -std=c++20 -Wall -Wextra -Wno-missing-field-initializers ray-cube.cpp -o ray-cube -I$RAYLIB_DIR  -L$RAYLIB_DIR -lraylib -framework OpenGL -framework CoreFoundation -framework CoreGraphics -framework IOKit -framework AppKit
-./ray-cube
+clang++ -g -std=c++20 -Wall -Wextra -Wno-missing-field-initializers cotw.cpp -o cotw -I$RAYLIB_DIR  -L$RAYLIB_DIR -lraylib -framework OpenGL -framework CoreFoundation -framework CoreGraphics -framework IOKit -framework AppKit
+./cotw
